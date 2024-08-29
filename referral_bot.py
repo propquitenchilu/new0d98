4b7b90d8a74968c94846c9f9093015b8c6740d03
propquitenchilu <159139624+propquitenchilu@users.noreply.github.com>
2024-08-29 22:27:00 +0200

    Update referral_bot.py

--- referral_bot.py
+++ referral_bot.py
@@ -5,7 +5,7 @@ import sqlite3
 import os
 
 # Configuration
-TOKEN = 'your-bot-token-here'
+TOKEN = '7494153356:AAFQo6RDU7o_Cjn88uVqmwTFyQZaqoOn25k'
 CHANNEL_ID = '@pancake90x'
 
 # Logging configuration
